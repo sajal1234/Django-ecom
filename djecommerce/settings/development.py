034310da7b0014f6349a2dd3e9c46c4248a841bd
sajal1234 <sjljn123@gmail.com>
2024-09-23 12:23:15 +0530

    deploy ssh

--- djecommerce/settings/development.py
+++ djecommerce/settings/development.py
@@ -42,6 +42,5 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-ROOT_URLCONF = 'djecommerce.urls'
 STRIPE_PUBLIC_KEY = config('STRIPE_TEST_PUBLIC_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_TEST_SECRET_KEY')
